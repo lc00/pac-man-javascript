c3209b4f5d28d6eca4ae0cd69090cc5b04dda3e2
Leanne Cheung <leannecheung@Leannes-MacBook-Air.local>
2019-02-08 08:43:11 -0700

    circle move across screen with one keypress

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-// // https://valhead.com/2017/07/21/animating-with-css-variables/
 
-// //https://github.com/jackrugile/start-making-games/blob/gh-pages/src/js/G/ball.js
+// references:
+// https://valhead.com/2017/07/21/animating-with-css-variables/
+// https://github.com/jackrugile/start-making-games/blob/gh-pages/src/js/G/ball.js
 
 
 var [xPosCurrent,yPosCurrent,xPosNew, yPosNew, xpos, ypos] = [0,0,0,0, 0, 0];
@@ -11,39 +12,58 @@ const rootEl = document.documentElement;
 // returns the style or css of the root element
 const docStyle = rootEl.style
 
+
+let parentEl = document.querySelector('#container')
+
+console.dir('parentEl', parentEl)
+// let parentElWidth = parentEl.clientWidth
+// let parentElHeight = parentEl.clientHeight
+// let parentElOffsetWidth = parentEl.offsetWidth
+// let parentElOffsetHeight = parentEl.offsetHeight
+
+// console.log('parentElWidth', parentElWidth)
+// console.log('parentElHeight', parentElHeight)
+// console.log('parentElOffsetWidth', parentElOffsetWidth)
+// console.log('parentElOffsetHeight', parentElOffsetHeight)
+
 function moveIt() {
-   //apply the newly calculated positions via CSS custom properties each frame. SO FANCY
-  // if(xPosCurrent != xPosNew) {
-  //   xPosCurrent += xPosNew
-  //   console.log('moveIt xpos', xPosCurrent)
-  //   docStyle.setProperty('--mouse-x', xPosCurrent);
-  // }
-  // if(yPosCurrent != yPosNew) {
-  //   yPosCurrent += yPosNew
-  //   console.log('moveIt ypos', yPosCurrent)
-  //   docStyle.setProperty('--mouse-y', yPosCurrent);
-  // }
   console.log('xpos', xpos)
   console.log('ypos', ypos)
   docStyle.setProperty('--mouse-x', xpos);
   docStyle.setProperty('--mouse-y', ypos);
 
   // requestAnimationFrame(moveIt);
-  
+
 }
 
-moveIt()
+function wait() {
+
+}
 
 document.addEventListener('keydown', function(e){ 
   console.log('e.code.....', e.code)
   
-  
+
+  console.log('')
   switch (e.code) {
     case 'ArrowRight':
-      xpos += 100
-      if(xpos <= 0) xpos = 0
-      // console.log('xpos', xpos)
-      moveIt()
+    
+      // if(parentEl)
+   
+
+      let moveInterval = setInterval(() => {
+        if(xpos < 500) {
+          
+          xpos += 3
+          // wait()
+          moveIt()
+        }
+        else {
+          clearInterval(moveInterval)
+          moveInterval = 0
+          console.log('moveInterval', moveInterval)
+        }
+      }, 10)
       break  
       
     case 'ArrowLeft':
