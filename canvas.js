b7f6368424b8a7fcf61c8ba6cb3ae23f43802ac8
Leanne Cheung <leannecheung@Leannes-MacBook-Air.local>
2019-02-17 17:17:48 -0700

    one circle instance moves

--- canvas.js
+++ canvas.js
@@ -35,45 +35,106 @@ let c = canvas.getContext('2d')
 // let radius = 30
 
 // for(let i = 0; i < 1; i++) {
-//     let x = Math.random() * canvas.width
-//     let y = Math.random() * canvas.height
-//     let radius = Math.random() * 50
+    // let x = Math.random() * canvas.width
+    // let y = Math.random() * canvas.height
+    // let dx = 5
+    // let dy = 5
+    // let radius = 30
+    // let x = 200
+    // let y = 200
+    // let radius = Math.random() * 50
 
-//     // c.beginPath()
-//     // c.arc(x, y, radius, 0, Math.PI * 2, false)
-//     // c.strokeStyle = 'blue'
-//     // c.stroke()
+    // c.beginPath()
+    // c.arc(x, y, radius, 0, Math.PI * 2, false)
+    // c.strokeStyle = 'blue'
+    // c.stroke()
 
-//     // animate(x,y, radius)
+    // animate(x,y,radius)
     
 // }
 
-let x = 200
-let y = 200
+const pauseTime = 50
+
+
+function pause() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, pauseTime)
+    })
+  }
+
+class Circle {
+    constructor(x,y,radius) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.dx = 5
+        this.dy = 5
+    
+
+    this.draw = function() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.strokeStyle = 'blue'
+        c.stroke()
+        console.log('draw...')
+        console.log('x', this.x)
+        console.log('y', this.y)
+        console.log('dx', this.dx)
+        console.log('dy', this.dy)
+
+        // await pause()
+        // this.update()
+    
+    }
+
+    this.update = function() {
+        c.clearRect(0, 0, innerWidth, innerHeight)
+        console.log('update...')
+
+        this.x = this.x + this.dx
+        this.y = this.y + this.dy
+
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0){
+            this.dx = - this.dx
+            console.log('hi, dx')
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0){
+            this.dy = - this.dy
+            console.log('hi, dy')
+
+        }
+
+
+        this.draw()
+    }
+}
+
+    
+}
+
+
+
+
+let circle = new Circle(200, 200, 30)
+circle.draw()
+// circle.update()
+
+
+let x = Math.random() * canvas.width
+let y = Math.random() * canvas.height
 let dx = 5
 let dy = 5
 let radius = 30
+// let radius = Math.random() * 50
 
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, innerWidth, innerHeight)
 
-    c.beginPath()
-    c.arc(x, y, radius, 0, Math.PI * 2, false)
-    c.strokeStyle = 'blue'
-    c.stroke()
-    console.log('ahh')
-    console.log('innerwidth', innerWidth)
-    x = x + dx
-    y = y + dy
+    circle.update()
 
-    if (x + radius > innerWidth || x - radius < 0){
-        dx = -dx
-    }
-    if (y + radius > innerHeight || y - radius < 0){
-        dy = -dy
-    }
-    console.log('x', x)
+
 }
 
-// animate()
+animate()
