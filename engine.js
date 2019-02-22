804d728651ece76417e54a24d955af2fe363090f
Leanne Cheung <leannecheung@Leannes-MacBook-Air.local>
2019-02-21 17:34:24 -0700

    pac-man keeps moving in update

--- engine.js
+++ engine.js
@@ -2,10 +2,12 @@
 // const {Player} = require('./player.js')
 
 let canvas = document.querySelector('canvas')
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 let c = canvas.getContext('2d')
-const pauseTime = 1000
+
+const pauseTime = 5000
+let direction
 
 class Character {
   constructor(speed) {
@@ -33,7 +35,21 @@ class Character {
           case 'down':
               this.yPos += this.speed
               break
+          case 'stop':
+              break
       }
+
+      if(this.xPos >= innerWidth) {
+        this.xPos -= this.speed
+      } else if (this.xPos <= 0) {
+        this.xPos += this.speed 
+      } 
+      if(this.yPos >= innerHeight) {
+        this.yPos -= this.speed
+      } else if (this.yPos <= 0) {
+        this.yPos += this.speed 
+      } 
+
       return {
           xPos: this.xPos,
           yPos: this.yPos
@@ -42,10 +58,11 @@ class Character {
 }
 
 class Player extends Character{
-  constructor(speed, xPos, yPos) {
+  constructor(speed, xPos, yPos, direction) {
       super(speed)
       this.xPos = xPos
       this.yPos = yPos
+      this.direction = direction
   }
 
   eat() {
@@ -70,58 +87,14 @@ class Engine {
 
     // setup
     setUp() {
-      this.player = new Player(10, 0, 0)
+      this.player = new Player(10, 200, 200, 'right')
       
     
     }  
 
     // draw
-    draw() {
-
-      let direction
-
-      document.addEventListener('keydown', e => { 
-        console.log('e.code.....', e.code)
-
-        switch (e.code) {
-          case 'ArrowRight':
-            direction = 'right'
-            break  
-          case 'ArrowLeft':
-            direction = 'left'
-            break
-          case 'ArrowUp':
-            direction = 'up'
-            break
-          case 'ArrowDown':
-            direction = 'down'
-            break
-          case 'Escape':
-          case 'KeyS':
-          case 'Enter':
-            direction = 'stop'
-            break
-          default:
-            break   
-        }
-
-        let pos 
-        // update position for pac-man
-        while (true) {
-        // await pause()
-
-          if(direction === 'stop')
-            break
-
-          pos = this.player.move(direction)
-          this.player.xPos = pos.xPos
-          this.player.yPos = pos.yPos
-          console.log(`xPos: ${this.player.xPos}, yPos: ${this.player.yPos}`)
-        
-          
-        
-        }
-      })
+    draw() { 
+      // c.clearRect(0, 0, innerWidth, innerHeight)
 
       c.beginPath()
       c.arc(this.player.xPos, this.player.yPos, 30, 0, Math.PI * 2, false)
@@ -132,8 +105,53 @@ class Engine {
     }
 
     // let arr = []
+
     update() {
+      // c.clearRect(0, 0, innerWidth, innerHeight)
+
       // udpate position for player
+      document.addEventListener('keydown', async(e) => { 
+        console.log('e.code.....', e.code)
+
+        switch (e.code) {
+          case 'ArrowRight':
+            this.player.direction = 'right'
+            break  
+          case 'ArrowLeft':
+            this.player.direction = 'left'
+            break
+          case 'ArrowUp':
+            this.player.direction = 'up'
+            break
+          case 'ArrowDown':
+            this.player.direction = 'down'
+            break
+          case 'Escape':
+          case 'KeyS':
+          case 'Enter':
+            this.player.direction = 'stop'
+            break
+          default:
+            break   
+        }
+      })
+
+      let pos 
+      // update position for pac-man
+      pos = this.player.move(this.player.direction)
+      this.player.xPos = pos.xPos
+      this.player.yPos = pos.yPos
+      console.log(`xPos: ${this.player.xPos}, yPos: ${this.player.yPos}`)
+    
+      
+
+      this.draw()
+
+
+
+
+
+
       // c.clearRect(0, 0, innerWidth, innerHeight)
       // requestAnimationFrame(update)
 
@@ -150,11 +168,20 @@ class Engine {
       // update position for other objects
     
       
-      this.draw()
+      // this.draw()
       
       }
 
     
+
+}
+
+function animation() {
+  c.clearRect(0, 0, innerWidth, innerHeight)
+
+  requestAnimationFrame(animation)
+
+  engine.update()
 
 }
 
@@ -163,6 +190,6 @@ class Engine {
 
 let engine = new Engine() 
 engine.setUp()
-// engine.update()
-
-engine.animate()
+// engine.draw()
+engine.update()
+animation()
