9ef27e9f3bb0a109f23aaeb0c3ae7907c01a9cc0
Leanne Cheung <leannecheung00@gmail.com>
2019-06-11 18:28:28 -0600

    separated character class into its own file

--- engine.js
+++ engine.js
@@ -1,6 +1,9 @@
 // import {Player} from './player'
 // const {Player} = require('./player.js')
 
+// const Character = require('./class.js')
+// import {Character} from './character.js'
+
 let canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
@@ -21,55 +24,7 @@ const smPelletRadius = 5
 const bigPelletRadius = 10
 
 
-class Character {
-  constructor(speed) {
-      this.speed = speed
-  }
 
-  get position () {
-      return {
-          xPos: this.xPos,
-          yPos: this.yPos
-      }
-  }
-
-  // pac-man can only move to a cell that
-  // has either pellet or empty cell
-  // so it checks the next cell that's in 
-  // the direction and see if it has pellet or is empty 
-  // if yes, it's move-able, 
-  // else stop in the current cell
-  move(direction) {
-      // let str = this.xPos + ',' + this.yPos
-      
-      // temp
-      let deltaX = 50
-      let deltaY = 50
-      let cell
-      switch (direction) {
-          case 'left':
-            this.xPos -= this.speed
-            break
-          case 'right':
-            this.xPos += this.speed
-            break
-          case 'up':
-            this.yPos -= this.speed
-            break
-          case 'down':
-            this.yPos += this.speed
-            break
-          case 'stop':
-            break
-      }
-
-      
-      return {
-          xPos: this.xPos,
-          yPos: this.yPos
-      }
-  }
-}
 
 class Player extends Character{
   constructor(speed, xPos, yPos, direction) {
@@ -655,14 +610,6 @@ let engine = new Engine()
 engine.setUp()
 engine.listen()
 engine.draw()
-// engine.update()
-// engine.update()
-// engine.update()
-// engine.update()
-// engine.update()
-// engine.update()
-// engine.update()
-// engine.update()
 
 animation()
 
