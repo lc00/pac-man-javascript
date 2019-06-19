fe608d723273cd8b62481cfed6594d5f2911e8bf
Leanne Cheung <leannecheung00@gmail.com>
2019-06-19 16:58:50 -0600

    grid drawn

--- mapGenerator/mapGenerator.js
+++ mapGenerator/mapGenerator.js
@@ -1,43 +1,33 @@
 const main = document.querySelector('main');
 const generateButton = document.querySelector('button#generateGrid');
 const canvas = document.querySelector('canvas');
-const canvasWidth = 10;
-const canvasHeight = 10;
-const canvasStartPoint = {x: 10, y: 10};
+const canvasWidth = 500;
+const canvasHeight = 500;
 const canvasHelper = CanvasHelper(canvas, canvasWidth, canvasHeight);
 
 generateButton.addEventListener('click', generateGrid);
 
 function generateGrid() {
     // grab the width and height in cell count
-    const horizontalCellCount = document.querySelector('#horizontalNum').value
+    const horizontileCellCount = document.querySelector('#horizontalNum').value
     const verticalCellCount = document.querySelector('#verticalNum').value
+    const width = canvasWidth / horizontileCellCount
+    const height = canvasWidth / verticalCellCount
+    let startPtX = 10
+    let startPtY = 10
 
-    //check that the canvasWidth and canvasHeight don't get to the limit
-    let startPtX = canvasStartPoint.x
-    let startPtY = canvasStartPoint.y
-    let rectangleWidth = canvasWidth / horizontalCellCount
+  for (let i=0; i< verticalCellCount; i++){
+    for(let j=0; j<horizontileCellCount; j++) {
 
-    // for(let j=1; j <= horizontalCellCount; j++) {
-      for (let i = 1; i <= horizontalCellCount; i++) {
-        console.log('startPtX', startPtX, 'startPtY', startPtY)
-  
-        canvasHelper.rectangle({
+      canvasHelper.rectangle({
           x: startPtX,
           y: startPtY, 
-          width: rectangleWidth, 
-          height: rectangleWidth
-        });
-  
-        startPtX += rectangleWidth
-        // startPtY += rectangleWidth
-  
-      }
+          width: width, 
+          height: height});
 
-      // startPtY += rectangleWidth
-  
-    // }
-    
-
-
+      startPtX += width
+    }
+    startPtX = 10
+    startPtY += height
+  }
 }
